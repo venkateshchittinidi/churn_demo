121f76bab69a0752d29b3af4b1b4033119b55eb9
Teja <venkateshchittinidi@gmail.com>
2021-12-07 19:05:02 +0530

    data changes

--- app.py
+++ app.py
@@ -12,15 +12,21 @@ adaboost = pickle.load(open('model_pkl', 'rb'))
 def home():
     return render_template("homepage.html")
 
+
+
 def get_data():
-    Tenure = request.form.get('Tenure')
-    MonthlyCharges = request.form.get('MonthlyCharges')
-    TotalCharges = request.form.get('TotalCharges')
+    SeniorCitizen_data = request.form.get('SeniorCitizen')
+    SeniorCitizen = int(SeniorCitizen_data)
+    Partner_data = request.form.get('Partner')
+    Partner = int(Partner_data)
+    Tenure_data = request.form.get('Tenure')
+    MonthlyCharges_data = request.form.get('MonthlyCharges')
+    TotalCharges_data = request.form.get('TotalCharges')
     Gender = request.form.get('Gender')
-    SeniorCitizen = request.form.get('SeniorCitizen')
-    Partner = request.form.get('Partner')
-    Dependents = request.form.get('Dependents')
-    PhoneService = request.form.get('PhoneService')
+    Dependents_data = request.form.get('Dependents')
+    Dependents = int(Dependents_data)
+    PhoneService_data = request.form.get('PhoneService')
+    PhoneService = int(PhoneService_data)
     MultipleLines = request.form.get('MultipleLines')
     InternetService = request.form.get('InternetService')
     OnlineSecurity = request.form.get('OnlineSecurity')
@@ -30,11 +36,29 @@ def get_data():
     StreamingTV = request.form.get('StreamingTV')
     StreamingMovies = request.form.get('StreamingMovies')
     #Churn = request.form.get('Churn')
-    PaperlessBilling = request.form.get('PaperlessBilling')
+    PaperlessBilling_data = request.form.get('PaperlessBilling')
+    PaperlessBilling = int(PaperlessBilling_data)
     PaymentMethod = request.form.get('PaymentMethod')
 
-    d_dict = {'Tenure': [Tenure], 'MonthlyCharges': [MonthlyCharges], 'TotalCharges': [TotalCharges], 'Gender_female': [0],
-              'Gender_male': [0], 'SeniorCitizen': [SeniorCitizen], 'Partner': [Partner], 'Dependents': [Dependents], 'PhoneService': [PhoneService],
+    Tenure = int(Tenure_data)
+    MonthlyCharges = int(MonthlyCharges_data)
+    TotalCharges = int(TotalCharges_data)
+
+
+    d_dict = {'SeniorCitizen': [SeniorCitizen],'Partner': [Partner],'Dependents': [Dependents],'Tenure': [Tenure], 'PhoneService': [PhoneService],
+              'PaperlessBilling': [PaperlessBilling],'MonthlyCharges': [MonthlyCharges],'TotalCharges': [TotalCharges],'Gender_female': [0],
+              'Gender_male': [0],'MultipleLines_no': [0], 'MultipleLines_nophoneservice': [0],'MultipleLines_yes': [0],
+              'InternetService_dsl': [0], 'InternetService_fiberoptic': [0],'InternetService_no': [0], 'OnlineSecurity_no': [0],
+               'OnlineSecurity_nointernetservice': [0],'OnlineSecurity_yes': [0],'OnlineBackup_no': [0], 'OnlineBackup_nointernetservice': [0],
+               'OnlineBackup_yes': [0],'DeviceProtection_no': [0], 'DeviceProtection_nointernetservice': [0],'DeviceProtection_yes': [0],
+               'TechSupport_no': [0], 'TechSupport_nointernetservice': [0],'TechSupport_yes': [0],'StreamingTV_no': [0], 'StreamingTV_nointernetservice': [0],
+               'StreamingTV_yes': [0],'StreamingMovies_no': [0], 'StreamingMovies_nointernetservice': [0],'StreamingMovies_yes': [0],
+               'PaymentMethod_banktransfer(automatic)': [0], 'PaymentMethod_creditcard(automatic)': [0],
+              'PaymentMethod_electroniccheck': [0], 'PaymentMethod_mailedcheck': [0]}
+
+
+    data = {'Tenure': [Tenure], 'MonthlyCharges': [MonthlyCharges], 'TotalCharges': [TotalCharges], 'Gender_female': [0],
+              'Gender_male': [0], 'SeniorCitizen': [SeniorCitizen],  'Dependents': [Dependents], 'PhoneService': [PhoneService],
               'MultipleLines_no': [0], 'MultipleLines_nophoneservice': [0],'MultipleLines_yes': [0], 
               'InternetService_dsl': [0], 'InternetService_fiberoptic': [0],'InternetService_no': [0], 
               'OnlineSecurity_no': [0], 'OnlineSecurity_nointernetservice': [0],'OnlineSecurity_yes': [0],
@@ -57,6 +81,9 @@ def get_data():
             d_dict[key] = 1
 
     print(d_dict,"afterrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr")
+    data = pd.DataFrame.from_dict(d_dict, orient='columns')
+    data.head()
+    print(data.dtypes)
     return pd.DataFrame.from_dict(d_dict, orient='columns')
 
 def feature_imp(model, data):
@@ -80,12 +107,9 @@ def show_data():
     #scaled_data = min_max_scale(featured_data)
     prediction = adaboost.predict(df)
     print(prediction,"ooooooooooooooooooutput!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    #outcome = 'Churner'
-    #if prediction == 0:
-    #outcome = 'Non-Churner'
 
-    #return render_template('results.html', tables = [df.to_html(classes='data', header=True)],result = prediction[0])
-    return render_template('results.html',result = prediction[0])
+    return render_template('results.html', tables = [df.to_html(classes='data', header=True)],result = prediction[0])
+    #return render_template('results.html',result = prediction[0])
 
 
 
